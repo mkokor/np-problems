75d15b801bb7fe633ea751ba4b81b212840056cf
Matija Kokor <matijakokorr@gmail.com>
2023-12-22 23:51:10 +0100

    more test on formula satisfiability check added

--- BooleanSatisfiabilityProblem.Tests/FormulaSatisfiabilityTest.cs
+++ BooleanSatisfiabilityProblem.Tests/FormulaSatisfiabilityTest.cs
@@ -24,5 +24,37 @@ namespace BooleanSatisfiabilityProblem.Tests
             Assert.IsTrue(threeCnfFormula.IsFormulaSatisfiable());
             CollectionAssert.AreEqual(actualResult, expectedResult);
         }
+
+        [TestMethod]
+        public void IsFormulaSatisfiable_ThreeFalseVariables_ReturnsCorrectResult()
+        {
+            var threeCnfFormula = new ThreeCnfFormula();
+            threeCnfFormula.SetFormula(new List<List<int>>()
+            {
+                new() { -1, -1, -1 },
+                new() { -2, -2, -2 },
+                new() { -3, -3, -3 }
+            });
+
+            List<bool> actualResult = new() { true, true, true };
+            threeCnfFormula.GetInterpretation(actualResult);
+            List<bool> expectedResult = new() { false, false, false };
+
+            Assert.IsTrue(threeCnfFormula.IsFormulaSatisfiable());
+            CollectionAssert.AreEqual(actualResult, expectedResult);
+        }
+
+        [TestMethod]
+        public void IsFormulaSatisfiable_UnsatisfiableFormula_ReturnsFalse()
+        {
+            var threeCnfFormula = new ThreeCnfFormula();
+            threeCnfFormula.SetFormula(new List<List<int>>()
+            {
+                new() { -1, -1, -1 },
+                new() { 1, 1, 1 }
+            });
+
+            Assert.IsFalse(threeCnfFormula.IsFormulaSatisfiable());
+        }
     }
 }
