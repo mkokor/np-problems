9198f8c7c38101e495829c0e5708499d1c2c4592
Matija Kokor <matijakokorr@gmail.com>
2023-12-23 10:51:33 +0100

    code refactored

--- CliqueIndependentSetGraph.Tests/GraphInputTest.cs
+++ CliqueIndependentSetGraph.Tests/GraphInputTest.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BooleanSatisfiabilityProblem.Exceptions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CliqueIndependentSetGraph.Tests
 {
